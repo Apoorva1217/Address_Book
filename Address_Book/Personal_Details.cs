137c055b08e5fb57fadd6a670dd565330b3e7872
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-03 18:11:11 +0530

    [Apoorva] Add . Ability to create contacts

---
+++ Address_Book/Personal_Details.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace Address_Book
+{
+    public class Personal_Details
+    {
+        /// <summary>
+        /// Ability to Create Contacts in Address Book
+        /// </summary>
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private string zipCode;
+        private string phoneNumber;
+        private string emailID;
+
+        public Personal_Details(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNumber, string emailID)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+            this.phoneNumber = phoneNumber;
+            this.emailID = emailID;
+        }
+
+        public string FirstName { get => this.firstName; set => this.firstName = value; }
+        public string LastName { get => this.lastName; set => this.lastName = value; }
+        public string Address { get => this.address; set => this.address = value; }
+        public string City { get => this.city; set => this.city = value; }
+        public string State { get => this.state; set => this.state = value; }
+        public string ZipCode { get => this.zipCode; set => this.zipCode = value; }
+        public string PhoneNumber { get => this.phoneNumber; set => this.phoneNumber = value; }
+        public string EmailID { get => this.emailID; set => this.emailID = value; }
+
+        public override string ToString()
+        {
+            return "\n  FirstName    : " + this.firstName
+                 + "\n  LastName     : " + this.lastName
+                 + "\n  Address      : " + this.address
+                 + "\n  City         : " + this.city
+                 + "\n  State        : " + this.state
+                 + "\n  ZipCode      : " + this.zipCode
+                 + "\n  PhoneNumber  : " + this.phoneNumber
+                 + "\n  EmailID      : " + this.emailID + "\n" +
+                   "/*************************/";
+        }
+    }
+}
